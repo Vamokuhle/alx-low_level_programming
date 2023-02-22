638366bb74370b6c3081455870e09b9b30ca1db7
Vamokuhle <vamokuhle.sithebe>
2023-02-22 13:57:32 -0800

    1-5

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (while count++ <= 9)
+	{
+		for (letter ='a'; letter <= 'z'; letter++0
+				_putchar(letter);
+		_putchar('\n');
+}
